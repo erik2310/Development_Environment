c2ef12fff7cca426044c0cb86adaa1127e79965d
Erik Ruhmanis <erik2310@edu.easj.dk>
2019-02-28 22:09:06 +0100

    Ændret titel til Beta branch.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Master branch' });
+  res.render('index', { title: 'Beta branch' });
 });
 
 module.exports = router;
